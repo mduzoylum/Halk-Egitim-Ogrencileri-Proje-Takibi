bb240eeab4eebadfe00b38ffb2bd9660bda9b600
muratbaz <33982093+muratbaz@users.noreply.github.com>
2017-11-28 13:26:06 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>Merhaba Komek</title>
+</head>
+<body>
+	<?php
+  $isim="Komek";
+  echo $isim;
+  ?>
+</body>
+</html>
